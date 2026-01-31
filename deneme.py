aadf470ba375cfc500348b1c5069d9262f2ecdfe
PapiLLeon <yilmazufukyasin@gmail.com>
2026-01-31 21:53:07 +0300

    Python yaş kontrolü sadeleştirildi

--- deneme.py
+++ deneme.py
@@ -1,3 +1,11 @@
 print("Git denemesi")
 print("Git öğreniyorum")
 print("feature brench icindeyim")
+isim = input("İsminizi Giriniz: ")
+yas = int(input("Yasinizi giriniz: "))
+print("Merhaba",isim)
+if yas >=18: 
+ print("Yetişkin")
+else:
+ print("Çocuk")
+
