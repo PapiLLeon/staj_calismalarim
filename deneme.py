a80dbb0c4e99375f1aea935330186ceadd928741
PapiLLeon <yilmazufukyasin@gmail.com>
2026-01-31 21:32:48 +0300

    Python if ve input alistirmasi

--- deneme.py
+++ deneme.py
@@ -1,3 +1,27 @@
 print("Git denemesi")
 print("Git öğreniyorum")
 print("feature brench icindeyim")
+print("Merhaba Python,satj basliyor")
+print("Merhaba")
+isim="Ufuk"
+print(isim)
+yas = 22
+print("Yas:",yas)
+if yas >= 18:
+    print("Yetiskin")
+else:
+    print("Cocuk")
+yas = int(input("Yasinizi girin: "))
+if yas >= 18:
+    print("Yetiskin")
+else:
+    print("Cocuk")
+isim = input("İsminizi Giriniz: ")
+print("Merhaba",isim)
+isim = input("İsminizi girin: ")
+yas= int(input("Yaşinizi girin: "))
+print("Merhaba",isim)
+if yas >= 19:
+    print("Yetişkin")
+else:
+    print("Çocuk")
