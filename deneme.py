947ef3810fc41d8dc7f4422efcc0266bd1ca91b5
PapiLLeon <yilmazufukyasin@gmail.com>
2026-02-01 23:43:42 +0300

    Python örnekler yapıldı

--- deneme.py
+++ deneme.py
@@ -8,5 +8,23 @@ if yas >=18:
  print("Yetişkin")
 else:
  print("Çocuk")
+isim = input("İsminizi giriniz: ")
+yas = int (input("Yaşinizi giriniz: "))
+if yas >=18 :
+ print("Merhaba",isim,"Yetişkinsiniz")
+else:
+ print("Merhaba",isim,"Çocuksunuz")
+sayi = int(input("Sayi giriniz: "))
+if sayi == 0:
+ print("Sıfır")
+elif sayi > 0 :
+ print ("Pozitif")
+else:
+ print("Negatif")
+x = input("Bir şey yaz: ")
+if x.isdigit():
+ print("Sayi girdin")
+else:
+ print("Yazi girdin")
 
 
